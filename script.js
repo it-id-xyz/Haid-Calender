1fa683598b3f271c738d69c630daacc7f1580eee
official IT Smansaci <ayamsayrrr78@gmail.com>
2026-05-15 21:00:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Import Firebase SDK (Setup placeholder)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.x/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.x/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDnbgqXr1K13W1MYwNlO83vmgyL3K4-aPw",
